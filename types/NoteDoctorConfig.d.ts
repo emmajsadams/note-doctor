d62d0f00911d29985016a0958d63c91dcfe0b40f
Emma Cline <emma@cline.engineer>
2020-12-14 14:21:23 -0700

    add email config options

--- types/NoteDoctorConfig.d.ts
+++ types/NoteDoctorConfig.d.ts
@@ -1,3 +1,11 @@
 export default interface NoteDoctorConfig {
 	notesPath?: string
+	sendGridAPIKey?: string
+	emailRecipients?: [
+		{
+			email: string
+			notesPath: string
+		},
+	]
+	noteRepos?: string[]
 }
