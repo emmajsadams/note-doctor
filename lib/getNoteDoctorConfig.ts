0ca69b13e555f1c00aa463c22d370ec0481ad99b
Emma Cline <emma@cline.engineer>
2020-12-14 14:23:55 -0700

    change note doctor config to cache

--- lib/getNoteDoctorConfig.ts
+++ lib/getNoteDoctorConfig.ts
@@ -1,15 +1,21 @@
 import fs from 'fs'
 import NoteDoctorConfig from '../types/NoteDoctorConfig'
 
+let NOTE_DOCTOR_CONFIG: NoteDoctorConfig
+
 export default function getNoteDoctorConfig(): NoteDoctorConfig {
+	if (NOTE_DOCTOR_CONFIG) {
+		return NOTE_DOCTOR_CONFIG
+	}
+
 	try {
 		const config = JSON.parse(fs.readFileSync('noteDoctorConfig.json', 'utf-8'))
 		console.log(
 			'Found noteDoctorConfig.json. Prioritizing JSON config over cli',
 		)
-		return {
-			notesPath: config.notesPath,
-		} as NoteDoctorConfig
+		NOTE_DOCTOR_CONFIG = config as NoteDoctorConfig
+
+		return NOTE_DOCTOR_CONFIG
 	} catch (err) {
 		return {} as NoteDoctorConfig
 	}
